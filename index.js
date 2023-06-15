94f10a396a3be656cefcf52810a746ac9993926c
germanbuchmuller <german.buchmuller@ing.austral.edu.ar>
2023-06-15 15:22:09 -0300

    Prisma integrated

--- index.js
+++ index.js
@@ -2,23 +2,47 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+const cors = require("cors");
 const { API_PORT } = process.env;
 app.use(bodyParser.json());
 
-let lastStatusMessage = null;
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
-app.get('/status', (req, res) => {
-  if (lastStatusMessage) {
-    res.json(lastStatusMessage);
-  } else {
-    res.status(404).json({ error: 'No hay un mensaje de estado disponible' });
+app.get('/status/latest', async(req, res) => {
+  try{
+    const lastStatus = await prisma.statusMessage.findFirst({
+      orderBy: { createdAt: 'desc' },
+    });
+    if (lastStatus) {
+      res.json(lastStatus.status);
+    } else {
+      res.status(404).json({ error: 'No hay un mensaje de estado disponible' });
+    }
+  }catch(e){
+    res.status(500).send("Internal server error");
   }
 });
 
-app.post('/status', (req, res) => {
-  const statusMessage = req.body;
-  lastStatusMessage = statusMessage;
-  res.json({ message: 'Mensaje de estado recibido correctamente' });
+app.post('/status', async(req, res) => {
+  const { status } = req.body;
+
+  try {
+    const createdStatus = await prisma.statusMessage.create({
+      data: { status },
+    });
+
+    res.json(createdStatus);
+  } catch (error) {
+    console.log('Error al guardar el mensaje de estado:', error);
+    res.status(500).json({ error: 'Error al guardar el mensaje de estado' });
+  }
 });
 
 app.listen(API_PORT, () => {
